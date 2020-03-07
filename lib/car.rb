84dda1533a123b78c72781edb5009c7ec8b37dc2
ljubicasekuloska <ljubicasekuloska@gmail.com>
2020-03-07 10:33:30 +0100

    Add new methods add_defect and service_info

--- lib/car.rb
+++ lib/car.rb
@@ -1,25 +1,36 @@
-#require 'car/version'
-
 module Car
   class MyCar
     attr_accessor :color, :current_speed
-    attr_reader :year
+    attr_reader :year, :defects
+    SPEED_OFFSET = 15
 
     def initialize(year, color, model)
       @year = year
       @color = color
       @model = model
       @current_speed = 0
+      @defects = []
     end
 
-    # speed_up verzija 1
-    def speed_up
-      self.current_speed += 10
+    def speed_up(inc = SPEED_OFFSET)
+      @current_speed += inc
     end
-    # speed_up verzija 2
-    # def speed_up(speed)
-    #   self.current_speed += speed
-    # end
 
+    def slow_down(dec = SPEED_OFFSET)
+      @current_speed -= dec
+    end
+
+    def spray_paint(paint)
+      @color = paint
+    end
+
+    def add_defect(defect)
+      @defects.push(defect)
+    end
+
+    def service_info
+      defects = @defects.join(', ') #spojuva elementi od nizata so , megju niv
+      "Defects to be fixed: #{defects}"
+    end
   end
 end
