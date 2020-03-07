dde1ba6b91a7ab41ba888bcde46bd7efbe5aebe7
ljubicasekuloska <ljubicasekuloska@gmail.com>
2020-03-07 10:34:38 +0100

    Add tests for methods add_defect and service_info

--- spec/mycar_spec.rb
+++ spec/mycar_spec.rb
@@ -1,23 +1,52 @@
-
 RSpec.describe Car::MyCar do
-  it 'Creates instance of MyCar class' do
-    volvo = Car::MyCar.new(1984, 'blue', 'volvo')
+  let(:volvo) { Car::MyCar.new(1984, 'blue', 'volvo') }
+  let(:failure) { 'water pump failure, dead battery' }
+
+  it 'creates instance of MyCar class' do
     expect(volvo).to be_instance_of(Car::MyCar)
   end
 
-  it 'Has initial car speed set to 0' do
-    volvo = Car::MyCar.new(1984, 'blue', 'volvo')
+  it 'has initial car speed set to 0' do
     expect(volvo.current_speed).to eq(0)
   end
-  # test verzija 1
-  it "Speeds up the car by 10km/h" do
-      volvo = Car::MyCar.new(1984, 'blue', 'volvo')
-      expect {volvo.speed_up}.to change {volvo.current_speed}.by(10)
+
+  it 'speeds up the car by 10km/h by default' do
+    volvo.speed_up
+    expect(volvo.current_speed).to eq(Car::MyCar::SPEED_OFFSET)
   end
 
-#   test verzija 2
-#   it 'speeds up the car' do
-#     volvo = Car::MyCar.new(1984, 'blue', 'volvo')
-#     expect { volvo.speed_up(40) }.to change { volvo.current_speed }.by(40)
-#   end
+  it 'speeds up the car by input value' do
+    volvo.speed_up(40)
+    expect(volvo.current_speed).to eq(40)
+  end
+
+  it 'slows down the car by 10km/h by default' do
+    volvo.slow_down
+    expect(volvo.current_speed).to eq(- Car::MyCar::SPEED_OFFSET)
+  end
+
+  it 'slows down the car by input value' do
+    volvo.slow_down(40)
+    expect(volvo.current_speed).to eq(-40)
+  end
+
+  it "changes the color of the object" do
+    volvo.spray_paint(:silver)
+    expect(volvo.color).to eq(:silver)
+  end
+
+  it 'has no defects' do
+    expect(volvo.defects).to be_empty
+  end
+
+  it 'adds a defect to list' do
+    volvo.add_defect(failure)
+    expect(volvo.defects).to include(failure)
+  end
+
+  it 'lists all the defects' do
+    volvo.add_defect(failure)
+    volvo.add_defect('oil change')
+    expect(volvo.service_info).to eq("Defects to be fixed: #{failure}, oil change")
+  end
 end
